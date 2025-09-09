f03c14ea4912d3da35f883a17231879eef8c98ea
Kirill-reu <kirillpicher1@gmail.com>
2025-09-09 11:18:36 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,12 @@
+# Создание пустого списка на python
+
+li = ['a', '1', 'b', '2', 'c', '3'] 
+
+# Создание структуры stack на python
+stack = []  
+
+stack.append('a') 
+stack.append('b') 
+stack.append('c') 
+
+print(stack) 
