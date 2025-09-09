da030aad73735cae45015c223a6a2d55c0dd03d3
Kirill-reu <kirillpicher1@gmail.com>
2025-09-09 18:55:36 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,13 @@
+// Создание пустого списка на Java
+
+ArrayList<String> stack = new ArrayList<>()
+
+
+// Создание структуры stack на Java
+
+ArrayList<String> stack = new ArrayList<>()
+
+stack.add("1");
+stack.add("2");
+stack.add("3");
+System.out.println(stack); // вывод элементов
