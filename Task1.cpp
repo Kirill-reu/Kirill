97ed579f2f198d8dfc62fb3ad4368e022046ce2e
Кирилл <kirillpicher1@gmail.com>
2025-09-14 14:15:48 +0300

    Add files via upload

---
+++ Task1.cpp
@@ -0,0 +1,23 @@
+// Создание пустого списка на C++
+
+#include <list>
+std::list<int> stack = {}
+
+
+// Создание структуры stack на C++
+
+#include <iostream>
+#include <list>
+
+int main() {
+   std::list<int> stack;
+   stack.push(1);
+   stack.push(2);
+   stack.push(3);
+   for (int value : stack) {
+      std::cout << value << " ";
+   {
+   std::cout << std::endl;
+
+   return 0;
+}
